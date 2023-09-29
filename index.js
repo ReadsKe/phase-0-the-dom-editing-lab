319adfb188d4ca5ac711832b15313a6b7feae438
Samuel Kimote <readssamuel@.com>
2023-09-29 21:07:05 +0300

    complete

---
+++ index.js
@@ -0,0 +1,7 @@
+const content = document.getElementById("content");
+{
+    content.innerHTML = '<h1>My HTML adventure</h1> <p>This is some new content for the body.<strong>browser</strong> <em>By: Byron Q. Poodle, Esq., BA.</em><a href="https://developer.mozilla.org/en-US/docs/Web/HTML"> DOM</a></p>'
+}
+
+const table = document.getElementsByClassName("table");{}
+
